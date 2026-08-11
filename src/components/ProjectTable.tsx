dc9a635d358f17b26335dbe2f20fa11c2d6fd176
Sherry <131241296+sherry2k@users.noreply.github.com>
2026-08-11 15:40:00 +0400

    Update ProjectTable.tsx

--- src/components/ProjectTable.tsx
+++ src/components/ProjectTable.tsx
@@ -297,7 +297,7 @@ const visibleProjects = dataQualityFilter
     { key: "sno", label: "S.No", width: "w-14", sortable: false },
     { key: "ownerName", label: "Owner Name", width: "min-w-[160px]", sortable: true },
     { key: "projectNo", label: "Project No", width: "min-w-[120px]", sortable: true },
-    { key: "plotNo", label: "Plot No", width: "min-w-[100px]", sortable: true },
+    { key: "plotNo", label: "Plot No", width: "w-16", sortable: true },
     { key: "projectLocation", label: "Project Location", width: "min-w-[160px]", sortable: true },
     { key: "noc", label: "NOC", width: "min-w-[120px]", sortable: true },
     { key: "perspective3d", label: "3D Perspective", width: "min-w-[140px]", sortable: true },
